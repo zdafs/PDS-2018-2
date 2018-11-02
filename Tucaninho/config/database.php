7b32aad9ebba6e427280116f27de2c2d4842e52e
Lucca Carvalho <luccalafonte97@gmail.com>
2018-11-02 11:31:01 -0300

    Colocadas variaveis de ambiente para conexão com o BD

--- Tucaninho/config/database.php
+++ Tucaninho/config/database.php
@@ -31,6 +31,15 @@ return [
     |
     */
 
+    if(getenv('DATABASE_URL')===true){
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        $host = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $database = substr($url["path"], 1);
+    }
+
     'connections' => [
 
         'sqlite' => [
@@ -57,11 +66,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $host),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', $database),
+            'username' => env('DB_USERNAME', $username),
+            'password' => env('DB_PASSWORD', $password),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
