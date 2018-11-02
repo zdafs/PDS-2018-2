1039c892f82c06c25fea4e6c4cb344d7967dcddd
Lucca Carvalho <luccalafonte97@gmail.com>
2018-11-02 13:18:22 -0300

    Outro teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
 
     <!-- Custom styles for this template -->
-    <link href="/home/css/agency.min.css" rel="stylesheet">
+    <link href="{{asset('public/home/css/agency.min.css')}}" rel="stylesheet">
 
     <!-- Login popup style -->
     <link href="{{asset('css/myModal.css')}}" rel="stylesheet">
